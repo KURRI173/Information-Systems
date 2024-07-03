f658ea5bd075cff6f898cfba6f444d85334c649e
KURRI173 <kurriashok12@gmail.com>
2024-07-03 20:23:33 +0530

    database

--- server.py
+++ server.py
@@ -1,8 +1,12 @@
 from flask import Flask, request, jsonify, send_from_directory,render_template
 import os
-
+from pymongo import MongoClient 
 app = Flask(__name__)
 
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/mydatabase'
+
+mongo = MongoClient(app.config['MONGO_URI'])
+db = mongo.get_database()
 
 # Route for home
 @app.route('/')
