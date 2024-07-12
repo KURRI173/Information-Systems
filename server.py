6caa7ab850cf5ede1b07c7a5570178b37ef830b4
KURRI173 <kurriashok12@gmail.com>
2024-07-13 01:02:38 +0530

    resgistervalidation

--- server.py
+++ server.py
@@ -21,15 +21,24 @@ def login():
 @app.route('/register' ,methods=['GET','POST'])
 def register():
    if(request.method=='POST'):
-      name=request.args.get('name')
-      email=request.args.get('email')
-      password=request.args.get('password')
-      confirmPassword=request.args.get('confirmPassword')
+      name=request.form.get('name')
+      email=request.form.get('email')
+      password=request.form.get('password')
+      confirmPassword=request.form.get('confirmpassword')
+      print(name,email,password,confirmPassword) 
 
-      print(name,email,password,confirmPassword)   
    return render_template('register.html')
 
-
+def checkForValidation(name,email,password,confirmPassword):
+   if(name==""):
+      return "name is empty"
+   if(email==""):
+      return "name is empty"
+   if(password==""):
+      return "name is empty"
+   if(confirmPassword==""):
+      return "name is empty"
+   
 @app.route('/favourites')
 def favourites():
    return render_template('favourites.html')
